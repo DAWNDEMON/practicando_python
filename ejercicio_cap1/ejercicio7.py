58af2073aa59a854b584759ca3cdee710ae726f1
DAWNDEMON <dawndemon1994@gmail.com>
2018-03-23 20:35:52 -0300

    sentencia for

---
+++ ejercicio_cap1/ejercicio7.py
@@ -0,0 +1,8 @@
+#Midiendo cadenas de texto.
+palabras = ['gato', 'ventana', 'defenestrado']
+for p in palabras:
+	print(p, len(p))
+#esto teoricamente responderia
+#gato 4
+#ventana 7
+#defenestrado 12
